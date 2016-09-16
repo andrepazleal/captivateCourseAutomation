4996b54a80766d52d2546fd8dc973e124919ccfd
andrepazleal <andrepazleal@gmail.com>
2016-09-16 13:42:34 -0300

    Update courseScriptTR.js

--- courseScriptTR.js
+++ courseScriptTR.js
@@ -1,7 +1,6 @@
 /*PRODUCTION SCRIPT FOR COURSES*/
 $('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">');
 $('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/andrepazleal/captivateCourseAutomation/Teste/courseStyle.css">');
-console.log('andre')
 
 cp.movie.am.pauseCurrentSlideAudioForInteractiveClick();
 
