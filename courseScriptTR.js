6b9b26a4067e31a69e7752f3e7a3cb6f4db7c563
andrepazleal <andrepazleal@gmail.com>
2016-09-16 10:41:46 -0300

    Update courseScriptTR.js

--- courseScriptTR.js
+++ courseScriptTR.js
@@ -1,6 +1,7 @@
 /*PRODUCTION SCRIPT FOR COURSES*/
 $('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">');
 $('head').append('<link rel="stylesheet" type="text/css" href="https://moodle.dominiosistemas.com.br/assets/css/courseStyle.css">');
+console.log('andre')
 
 cp.movie.am.pauseCurrentSlideAudioForInteractiveClick();
 
