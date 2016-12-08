46aece9a947a57584578ffb02bf422950e302ab2
andrepazleal <andrepazleal@gmail.com>
2016-12-07 23:45:12 -0200

    Update courseScriptTR.js

--- courseScriptTR.js
+++ courseScriptTR.js
@@ -1,5 +1,4 @@
 /*TEST SCRIPT FOR COURSES*/
-console.log('andre');
 $('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">');
 $('head').append('<link rel="stylesheet" type="text/css" href="https://gitcdn.link/repo/andrepazleal/captivateCourseAutomation/Teste/courseStyle.css">');
 
